3dc159b58e46c357b64ada02f0d1df3b9b1e22af
rimonc7 <rimonc7@gmail.com>
2024-09-24 14:10:13 +0600

    project done

---
+++ js/blog.js
@@ -0,0 +1,7 @@
+document.getElementById('home-btn').addEventListener('click',function(){
+    window.location.href  = './index.html';
+})
+
+document.getElementById('home-btn-mob').addEventListener('click',function(){
+    window.location.href  = './index.html';
+})
